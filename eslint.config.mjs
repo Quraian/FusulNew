4545a208f9f6370f12549e83558aa90f1240a198
Mohammad AlQuraian <m4design@gmail.com>
2025-01-03 16:36:35 +0300

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,21 @@
 // @ts-check
 
+import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import sonarjs from 'eslint-plugin-sonarjs';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+// import pluginReact from 'eslint-plugin-react';
+
+const compat = new FlatCompat();
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  // pluginReact.configs.flat?.recommended, // doesn't work
+  ...compat.extends('plugin:react/recommended'),
+  ...compat.extends('plugin:react-hooks/recommended'),
   sonarjs.configs.recommended,
   {
     rules: {
@@ -22,5 +30,6 @@ export default tseslint.config(
       '/coverage/**/*',
     ],
   },
-  eslintConfigPrettier
+  eslintConfigPrettier,
+  jsxA11y.flatConfigs.recommended
 );
