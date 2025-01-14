ef4f605f76004509ae46bdf27848a2e9d4e352ea
Mohammad AlQuraian <m4design@gmail.com>
2025-01-14 18:02:56 +0300

    add vite

--- ToDos.md
+++ ToDos.md
@@ -1,3 +1,4 @@
+- use .xxx notation in scripts
 - unplugin-swc, not needed if we cover tests using e2e
   - also no need nock
 - [Vite coverage](https://vitest.dev/guide/coverage)
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,47 @@
 // @ts-check
 
-import { FlatCompat } from '@eslint/eslintrc';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import sonarjs from 'eslint-plugin-sonarjs';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import jsxA11y from 'eslint-plugin-jsx-a11y';
-// import pluginReact from 'eslint-plugin-react';
+// import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
-const compat = new FlatCompat();
+// export default tseslint.config({
+//   extends: [js.configs.recommended, ...tseslint.configs.recommended],
+//   files: ['**/*.{ts,tsx}'],
+//   languageOptions: {
+//     ecmaVersion: 2020,
+//     globals: globals.browser,
+//   },
+//   plugins: {
+//     'react-hooks': reactHooks,
+//     'react-refresh': reactRefresh,
+//   },
+//   rules: {
+//     ...reactHooks.configs.recommended.rules,
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// });
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
-  // pluginReact.configs.flat?.recommended, // doesn't work
-  ...compat.extends('plugin:react/recommended'),
-  ...compat.extends('plugin:react-hooks/recommended'),
   sonarjs.configs.recommended,
+  {
+    languageOptions: {
+      parserOptions: {
+        allowDefaultProject: ['*.js'],
+        defaultProject: 'tsconfig.json',
+      },
+    },
+  },
   {
     rules: {
       'sonarjs/todo-tag': 'off',
@@ -24,7 +49,7 @@ export default tseslint.config(
   },
   {
     ignores: [
-      '**/dist',
+      '**/dist/**/*',
       'packages/mobile/android/**/*',
       'packages/mobile/ios/**/*',
       '/coverage/**/*',
@@ -32,5 +57,23 @@ export default tseslint.config(
     ],
   },
   eslintConfigPrettier,
-  jsxA11y.flatConfigs.recommended
+  jsxA11y.flatConfigs.recommended,
+  {
+    files: ['packages/mobile/**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  }
 );
